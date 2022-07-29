0c65ef4d6064ed54064c54839f93c0134461733d
vishal <vishalrawtani@gmail.com>
2022-07-30 01:21:42 +0530

    Add polls CreateView

--- polls_app/urls.py
+++ polls_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PollsListView, PollsDetailView
+from .views import PollsListView, PollsDetailView, PollsCreateView
 
 urlpatterns = [
+    path("poll/new", PollsCreateView.as_view(), name="poll_new"),
     path("poll/<int:pk>", PollsDetailView.as_view(), name="poll_detail"),
     path("", PollsListView.as_view(), name="home"),
 ]
--- polls_app/views.py
+++ polls_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 
 from .models import Polls
 
@@ -13,3 +14,9 @@ class PollsDetailView(DetailView):
     model = Polls
     template_name = "poll_detail.html"
     context_object_name = "poll"
+
+
+class PollsCreateView(CreateView):
+    model = Polls
+    template_name = "polls_create.html"
+    fields = ["owner", "question", "choice1", "choice2", "choice3", "choice4"]
