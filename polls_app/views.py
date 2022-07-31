6a38dc2341069c7499ef80bd72bee83c03ce8608
vishal <vishalrawtani@gmail.com>
2022-07-31 18:01:35 +0530

    Add vote functionality

--- polls_app/views.py
+++ polls_app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView
+from django.http import HttpResponse
 
 from .models import Polls
 
@@ -30,5 +31,18 @@ class PollsCreateView(CreateView):
 
 def vote_view(request, poll_id):
     poll = Polls.objects.get(pk=poll_id)
+    if request.method == "POST":
+        selected_option = request.POST["optionsRadios"]
+        if selected_option == "option1":
+            poll.choice1_count += 1
+        elif selected_option == "option2":
+            poll.choice2_count += 1
+        elif selected_option == "option3":
+            poll.choice3_count += 1
+        else:
+            return HttpResponse(400, "Invalid form")
+
+        poll.save()
+        return redirect("result", poll_id)
     context = {"poll": poll}
     return render(request, "vote.html", context)
